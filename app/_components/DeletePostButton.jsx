b066776d5d1be1bb932a8258ffdcd8e750eeed8a
kimszin <kimszin@naver.com>
2024-05-28 22:34:16 +0900

    [Feat] init

---
+++ app/_components/DeletePostButton.jsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export default function DeletePostButton({ postId }) {
+  const router = useRouter();
+
+  async function handleClick() {
+    try {
+      await fetch(`api/post/${postId}`, {
+        method: "DELETE",
+      });
+      router.refresh();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return <button onClick={handleClick}>Delete Post</button>;
+}
